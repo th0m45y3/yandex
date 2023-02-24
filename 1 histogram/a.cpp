d59dacb5dcdf456ace3d45b83877f24fba0718ff
Thomas Edward <th0m45y3@gmail.com>
2023-02-24 10:56:45 +0000

    init

---
+++ 1 histogram/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main() {
+	int arr[94] = {};
+	std::ifstream input; 
+	input.open("input.txt");
+	std::string str;
+	std:: cin >> str;
+	std::cout << "hel";
+}
