71bb6c18d604243574d3a4e46c6868605c91c460
Thomas Edward <th0m45y3@gmail.com>
2023-02-25 18:53:31 +0000

    accepted solution !

--- 1 histogram/a.cpp
+++ 1 histogram/a.cpp
@@ -7,50 +7,35 @@
 int main() {
 	int arr[94] = {};
 	std::string str;
-	std::ifstream input ("input.txt");
+	char c;
+	std::ifstream input("input.txt");
 	if (input.is_open()) {
-		while(std::getline(input, str)) {
-			for (char c : str) {
-				int cc = int(c);
-				if ((cc > 32)&&(cc < 127)) {
-					arr[cc-33] += 1;
-				}
+		while(input.get(c)) {
+			int cc = int(c);
+			if (c != ' ' && c != '\n') {
+				arr[cc-33] += 1;
 			}
 		}
 		input.close();
 	}
-	std::vector <std::pair<int,int>> vec;
-	std::vector<int> pos;
-	str.clear();
-
-	for (int i = 0; i < 94; i ++){
-		if (arr[i] != 0) {
-			vec.push_back(std::make_pair(arr[i], i + 33));
-			pos.push_back(i + 33);
-			str += " ";
-		}
-	}
-	
-	sort(vec.rbegin(), vec.rend());
-	str += '\n';
 
 	std::ofstream output;
-  	output.open ("output.txt", std::ios::binary );
+  	output.open ("output.txt");
 	if (output.is_open()) {
-		for(int i = 0; i < static_cast<int>(vec.size()); i++) {
-			auto it = find(pos.begin(), pos.end(), vec[i].second);
-			int index = std::distance( pos.begin(), it );
-			str.replace(index, 1, "#");
-			for (int j = 0; j < vec[i].first - vec[i + 1].first; j++) {
-				output << str;
+		for(int i = *std::max_element(arr, arr + 94); i > 0; i--) {
+			for(int j : arr) {
+				if(j > 0) {
+					if (j < i) output << " ";
+					else output << "#";
+				}
 			}
+			output << "\n";
 		}
-		output << std::string(static_cast<int>(vec.size()), '#') << '\n';
-		for(auto i : pos) {
-			output << char(i);
-		}
-		output << '\n';
 	}
+	for(int i = 0; i < 94; i++) {
+		if (arr[i] > 0) output << static_cast<char>(i + 33);
+	}
+	output << "\n";
   	output.close();
 
 	return 0;
